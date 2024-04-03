2b791537fe3a9f7560a2510539a2caa32905f7ed
akbarhadiq <akbarhadiq.ha@gmail.com>
2024-04-03 09:28:35 +0700

    Fix : Fix Get All Member & Get Member By Id

--- controller/memberController.js
+++ controller/memberController.js
@@ -1,6 +1,37 @@
 const {PrismaClient} = require('@prisma/client')
 const prisma = new PrismaClient()
 
+const getAllMember = async(req,res) => {
+
+    try{
+        const allMember = await prisma.member.findMany()
+        res.json(allMember)
+    }
+
+    catch(err){
+        console.error(err)
+        res.status(500).json(err)
+    }
+   
+}
+
+const getMemberById = async(req,res)=>{
+    const id = req.params
+    try{
+        const member = await prisma.member.findMany({
+            where:{
+                member_id:id
+            }
+        })
+
+        res.json(member)
+    }
+    catch(err){
+        console.error(err)
+        res.status(500).json(err)
+    }
+}
+
 const createMember = async(req,res)=>{
     const {name} = req.body;
     const memberCount = await prisma.member.count();
