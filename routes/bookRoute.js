b492981c47fa47930e20a2c4c366cc0738217cec
akbarhadiq <akbarhadiq.ha@gmail.com>
2024-04-03 10:47:09 +0700

    Feat : Book Route Done

---
+++ routes/bookRoute.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const bookRouter = express.Router()
+
+const {getAllBooks, getBookById, addNewBook, updateBook, deleteBook} = require('../controller/booksController')
+
+bookRouter.get('/books', getAllBooks)
+bookRouter.get('/books/:id', getBookById)
+bookRouter.post('/books', addNewBook)
+bookRouter.patch('/books/:id', updateBook)
+bookRouter.delete('/books/:id', deleteBook)
