ce4aebc0f3edc1637b65bf33c36c1590ce1ef455
akbarhadiq <akbarhadiq.ha@gmail.com>
2024-04-04 17:00:37 +0700

    Fix : Fix

--- TEST ALGORITMA/no2.js
+++ TEST ALGORITMA/no2.js
@@ -2,7 +2,7 @@ const sentence = "Saya sangat senang mengerjakan soal algoritma"
 // console.log(sentences_array)
 
 function longest(sentences){
-    const sentences_array = sentence.split(" ")
+    const sentences_array = sentences.split(" ")
     let longest_index = 0
     for(let i=0; i<sentences_array.length; i++){
         if (sentences_array[i].length > sentences_array[longest_index].length){
